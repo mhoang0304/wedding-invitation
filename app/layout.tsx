efd248a4352d34bc7b78e055357cae2b6f88d309
Minh Hoang <hoang.pham3@ntq-solution.com.vn>
2025-12-13 00:38:51 +0700

    fix: 🐛 file preview

--- app/layout.tsx
+++ app/layout.tsx
@@ -30,7 +30,7 @@ export const metadata: Metadata = {
     siteName: "Wedding Invitation",
     images: [
       {
-        url: "https://wedding-invitation-12.vercel.app/preview_1200x630_v2.jpg",
+        url: "https://wedding-invitation-12.vercel.app/preview_1200x630_v2.jpg?v=2",
         width: 1200,
         height: 630,
         alt: "Wedding Invitation Preview",
